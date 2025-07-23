d169a9e696cc7ac9e7e6a9c72a3788edfee55d1f
retekant <retekant@gmail.com>
2025-07-23 06:46:46 -0700

    finalizing style changes

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -40,18 +40,18 @@ export default function LoginPage() {
     };
 
   return (
-    <div className="flex  bg-gray-800 w-full items-center justify-center h-screen ">
+    <div className="flex  bg-gray-900 w-full items-center justify-center h-screen ">
         <Navbar/>
-        <div className='min-w-1/3 min-h-1/2 bg-gray-600 rounded-md shadow p-12
-        flex flex-col justify-start items-center gap-6'>
+        <div className='min-w-1/3 min-h-1/2 bg-gray-800 rounded-md p-12
+        flex flex-col justify-start items-center gap-6 shadow-lg shadow-indigo-900/20'>
             <h2 className='text-xl font-bold text-shadow-2'>
                 Email: {user?.email}
             </h2>
              
 
             <button onClick={handleForgotPassword}
-                className='py-2 hover:bg-blue-900/50 bg-blue-800/50 w-1/3 rounded-md
-                transition-all duration-300'>Reset Password</button>
+                className='py-2 hover:bg-blue-500/50 bg-blue-400/50 w-1/3 rounded-md
+                transition-all duration-300 '>Reset Password</button>
 
                 
             <button onClick={handleSignOut}
