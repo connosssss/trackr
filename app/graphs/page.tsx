68105b4b08d12c904aaa13425c07d8de2ca89d4d
connosssss <connorgriffin2424@gmail.com>
2025-08-05 14:26:29 -0700

    structure for graphs page

---
+++ app/graphs/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import Navbar from "@/components/Navbar";
+
+
+
+export default function LoginPage() {
+
+  return (
+    <div className="flex  bg-gray-900 w-full items-center justify-center h-screen ">
+        <Navbar/>
+        
+  </div>
+  );
+}
