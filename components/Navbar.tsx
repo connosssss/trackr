001145d4ecc456d1485b6c023463650dd3386c13
connosssss <connorgriffin2424@gmail.com>
2025-10-21 13:37:11 -0700

    Navbar theme set up

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,23 +3,42 @@
 // import { useRouter } from "next/navigation";
 import Link from 'next/link';
 import { useState } from 'react'; 
+import {fetchUserTheme } from "@/utils/userSettings"
+import { useAuth } from "@/context/AuthContext";
 
 
 export default function Navbar() {
 
   const [isMinimized, setIsMinimized] = useState(true);
+  const [theme, setTheme] = useState("");
+  const { user} = useAuth();
+
+
+  const getTheme = async () => {
+    if(!user) throw "userError";
+
+    try{
+      let data = await fetchUserTheme(user.id);
+      setTheme(data);
+    }
+    catch(err){
+      alert("Error in loading theme / user for navbar: " +err)
+    }
+
+  }
 
   const toggleMinimize = () => {
     setIsMinimized(!isMinimized);
   };
 
   return (
-    <div className={`fixed right-0 h-screen backdrop-blur-sm z-20 bg-[#07060a]
+    <div className={`fixed right-0 h-screen backdrop-blur-sm z-20 ${theme == "default" ? "bg-[#07060a] text-white" : "bg-[#aab3bf] text-gray-800"}
     ${isMinimized ? 'w-16' : 'w-1/6'} transition-all duration-500 ease-in-out `}>
       <button 
         onClick={toggleMinimize} 
-        className={`absolute top-2 ${isMinimized ? 'left-2' : 'left-5'} bg-[#1B1A1F] hover:hover:bg-[#2A292E]
+        className={`absolute top-2 ${isMinimized ? 'left-2' : 'left-5'} 
          text-white rounded-md z-30 px-4 ${!isMinimized ? 'w-[80%]' : 'w-12'} mr-2
+         ${theme == "default" ? "bg-[#1B1A1F] hover:hover:bg-[#2A292E]" : "bg-[#8a94a1] hover:bg-[#6a7685]"}
          transition-all duration-500 ease-in-out`}
       >
         {isMinimized ? '→' : '←'}
